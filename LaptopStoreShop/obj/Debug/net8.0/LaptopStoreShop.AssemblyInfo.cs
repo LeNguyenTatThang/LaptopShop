0ab8f675aa9b758c5f896a90b536bb28782066c2
LeNguyenTatThang <lenguyentatthang.dev@gmail.com>
2025-02-27 13:34:38 +0700

    fix ckeditor

--- LaptopStoreShop/obj/Debug/net8.0/LaptopStoreShop.AssemblyInfo.cs
+++ LaptopStoreShop/obj/Debug/net8.0/LaptopStoreShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LaptopStoreShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8276685a29becf971eb941807bfbbed60e20b48c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ecc62e203a10f389293157b1e72f8a5def1e809")]
 [assembly: System.Reflection.AssemblyProductAttribute("LaptopStoreShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LaptopStoreShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
